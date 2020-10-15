f8dd05821a1a3386b8e00f0cfa5b3d3eb6674a3b
Emirhan Cihangir <cihangiremir@outlook.com>
2020-10-15 23:49:12 +0300

    v1

---
+++ BasiChat/Models/Chat.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasiChat.Models
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("GetConnectionId", this.Context.ConnectionId);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);//ReceiveMessage =>clientteki js methodunun adıdır.user ve message ise methodun aldığı parametrelerdir.
+        }
+    }
+}
